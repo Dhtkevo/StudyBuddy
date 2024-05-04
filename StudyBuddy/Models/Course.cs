f69a2937e2c3b6296a8d0f29cb659545ebfc64b9
km00372 <km00372@CSVLAB-VM10>
2024-05-04 11:05:44 -0400

    Add the course model class

---
+++ StudyBuddy/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyBuddy.Models
+{
+    /// <summary>
+    /// Model class that represents a course
+    /// </summary>
+    public class Course
+    {
+        public int Id { get; set; }
+        [Display(Name = "Course Name")]
+        [StringLength(30, MinimumLength = 10)]
+        [Required]
+        public string? Name { get; set; }
+
+        [StringLength(20, MinimumLength = 4)]
+        [Required]
+        public string? Subject { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
