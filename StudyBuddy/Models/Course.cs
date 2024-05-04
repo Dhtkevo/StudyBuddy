1f5e5e9c8ec1961dbc26dd41920e38010a20f410
km00372 <km00372@CSVLAB-VM10>
2024-05-04 11:31:39 -0400

    Document public members for courses

--- StudyBuddy/Models/Course.cs
+++ StudyBuddy/Models/Course.cs
@@ -7,16 +7,41 @@ namespace StudyBuddy.Models
     /// </summary>
     public class Course
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         [Display(Name = "Course Name")]
         [StringLength(30, MinimumLength = 10)]
         [Required]
         public string? Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the subject.
+        /// </summary>
+        /// <value>
+        /// The subject.
+        /// </value>
         [StringLength(20, MinimumLength = 4)]
         [Required]
         public string? Subject { get; set; }
 
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>        
         public string? Description { get; set; }
     }
 }
--- StudyBuddy/Models/CourseSubjectViewModel.cs
+++ StudyBuddy/Models/CourseSubjectViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Collections.Generic;
 
 namespace StudyBuddy.Models;
 
+/// <summary>
+/// View model for course subjects
+/// </summary>
 public class CourseSubjectViewModel
 {
+    /// <summary>
+    /// Gets or sets the courses.
+    /// </summary>
+    /// <value>
+    /// The courses.
+    /// </value>
     public List<Course>? Courses { get; set; }
+    /// <summary>
+    /// Gets or sets the subjects.
+    /// </summary>
+    /// <value>
+    /// The subjects.
+    /// </value>
     public SelectList? Subjects { get; set; }
+    /// <summary>
+    /// Gets or sets the course subject.
+    /// </summary>
+    /// <value>
+    /// The course subject.
+    /// </value>
     public string? CourseSubject { get; set; }
+    /// <summary>
+    /// Gets or sets the search string.
+    /// </summary>
+    /// <value>
+    /// The search string.
+    /// </value>
     public string? SearchString { get; set; }
 }
