02655d946dacc4abae0dceae14c12d8fa2144508
km00372 <km00372@CSVLAB-VM10>
2024-05-04 13:29:37 -0400

    Implement Model class for tasks

---
+++ StudyBuddy/Models/Task.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyBuddy.Models
+{
+    /// <summary>
+    /// Class that represents a task
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the task.
+        /// </summary>
+        /// <value>
+        /// The name of the task.
+        /// </value>
+        [Required]
+        public string? TaskName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        /// <value>
+        /// The created date.
+        /// </value>
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Gets or sets the end date.
+        /// </summary>
+        /// <value>
+        /// The end date.
+        /// </value>
+        public DateTime? EndDate { get; set; }
+    }
+}
